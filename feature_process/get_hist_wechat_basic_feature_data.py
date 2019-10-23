5d12fc83464905066c17d89449797341794c4450
zhangmeng <zhangmeng@sunlands.com>
2019-10-23 15:50:24 +0800

    init

--- feature_process/get_hist_wechat_basic_feature_data.py
+++ feature_process/get_hist_wechat_basic_feature_data.py
@@ -2,23 +2,23 @@
 """
 
 """
-from feature_process.get_one_day_wechat_full_sentence_data import get_one_day_wechat_full_sentence
+from feature_process.get_one_day_wechat_basic_feature import get_one_day_wechat_basic_feature
 from util.dateutil import DateUtil
 from log.get_logger import G_LOG as log
 
 
-def get_hist_wechat_full_sentence(start_date, end_date):
+def get_hist_wechat_basic_feature(start_date, end_date):
     date_ls = DateUtil.get_every_date(start_date, end_date)
     for tmp_date in date_ls:
         log.info("extract %s basic feature..." % tmp_date)
-        get_one_day_wechat_full_sentence(tmp_date)
+        get_one_day_wechat_basic_feature(tmp_date)
 
 
 def main():
     import sys
     start_date = sys.argv[1]
     end_date = sys.argv[2]
-    get_hist_wechat_full_sentence(start_date, end_date)
+    get_hist_wechat_basic_feature(start_date, end_date)
 
 
 if __name__ == "__main__":
