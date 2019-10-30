96ac34ee30d6c007524647905c44d55fbacfa835
zhangmeng <zhangmeng@sunlands.com>
2019-10-30 16:01:07 +0800

    auc save model;early stop;benchmark test

--- evaluate.py
+++ evaluate.py
@@ -19,8 +19,8 @@ pd.set_option('display.unicode.east_asian_width', True)
 np.set_printoptions(threshold=np.inf)
 
 
-def evaluate_model(model_saved_path, valid_tf_record_path, start_date, end_date):
-    assert os.path.isdir(model_saved_path) and os.path.isdir(valid_tf_record_path)
+def evaluate_model(model_saved_path, test_tf_record_path, start_date, end_date):
+    assert os.path.exists(model_saved_path) and os.path.isdir(test_tf_record_path)
 
     # 加载验证数据
     tfRecorder = tf_recorder.TFRecorder()
@@ -28,23 +28,24 @@ def evaluate_model(model_saved_path, valid_tf_record_path, start_date, end_date)
     label_name = "label"
     padding = ({"chat_content": [SEQUENCE_MAX_LEN]}, [None])
 
-    valid_dataset = tfRecorder.get_dataset_from_path(valid_tf_record_path, feature_ls, label_name=label_name,
+    valid_dataset = tfRecorder.get_dataset_from_path(test_tf_record_path, feature_ls, label_name=label_name,
                                                      start_date=start_date, end_date=end_date,
                                                      batch_size=args.batch_size, padding=padding)
 
     model = keras.models.load_model(model_saved_path)
     result = model.evaluate(valid_dataset)
-    print("evaluate result:\n", result)
+    print("evaluate result for %s:\n" % os.path.basename(test_tf_record_path), result)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="textCnn model..")
+    parser = argparse.ArgumentParser(description="evaluate model..")
     parser.add_argument("-batch_size", "--batch_size", type=int, default=256, help="batch_size")
     parser.add_argument("-model_saved_path", "--model_saved_path", type=str, default="", help="model_saved_path")
     parser.add_argument("-valid_tf_record_path", "--valid_tf_record_path", type=str, default="",
                         help="valid_tf_record_path")
     parser.add_argument("-start_date", "--start_date", type=str, default="20190801", help="start_date")
     parser.add_argument("-end_date", "--end_date", type=str, default="20190930", help="end_date")
+    parser.add_argument("-local_mode", "--local_mode", type=bool, default=True, help="local_mode")
 
     args = parser.parse_args()
     print("Argument:", args, "\n")
@@ -53,4 +54,8 @@ if __name__ == '__main__':
     assert os.path.isdir(args.valid_tf_record_path)
     assert args.start_date <= args.end_date and "" not in [args.start_date, args.end_date]
 
-    evaluate_model(args.model_saved_path, args.valid_tf_record_path, args.start_date, args.end_date)
+    if args.local_mode:
+        evaluate_model("E:\\project_data\\text_cnn_demo\\result\\20191030-11-44\\Model_Saved",
+                       "E:\\project_data\\text_cnn_demo\\tf_record\\wechat_basic_feature", "20190503", "20190503")
+    else:
+        evaluate_model(args.model_saved_path, args.valid_tf_record_path, args.start_date, args.end_date)
--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import os
 import pandas as pd
 import numpy as np
 from config.global_config import *
+from evaluate import evaluate_model
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -19,26 +20,36 @@ pd.set_option('display.unicode.east_asian_width', True)
 np.set_printoptions(threshold=np.inf)
 
 
-def training(train_dataset, valid_dataset, vocab_size, epochs, model_saved_path):
+def training(train_dataset, valid_dataset, vocab_size, epochs, model_saved_path, log_path):
     model = TextCnn(args.feature_size, args.embedding_size, vocab_size, args.classes_num, args.filter_num,
                     args.filter_list, args.drop_out_ratio)
     model.compile(tf.keras.optimizers.Adam(), loss=keras.losses.BinaryCrossentropy(),
-                  metrics=[keras.metrics.AUC(), keras.metrics.binary_accuracy, keras.metrics.Recall(),
-                           keras.metrics.Precision()])
+                  metrics=[keras.metrics.AUC(), keras.metrics.binary_accuracy, keras.metrics.Precision(),
+                           keras.metrics.Recall()])
     model.summary()
 
-    # history = model.fit(train_dataset,validation_data = valid_dataset,use_multiprocessing=True,validation_steps=100,steps_per_epoch=200)
-    history = model.fit(train_dataset, validation_data=valid_dataset, epochs=epochs)
+    callbacks = [
+        # Interrupt training if `val_loss` stops improving for over 2 epochs
+        keras.callbacks.EarlyStopping(monitor='val_loss', patience=2),
+        # Write TensorBoard logs to logs path, terminal use: tensorboard --logdir=...
+        keras.callbacks.TensorBoard(log_dir=log_path)
+    ]
+
+    history = model.fit(train_dataset, validation_data=valid_dataset, epochs=epochs, callbacks=callbacks)
+
+    print("Log path:", log_path)
     print("Save model:", model_saved_path)
-    keras.models.save_model(model, model_saved_path, save_format='tf')
+    keras.models.save_model(model, model_saved_path, save_format='h5')
+
     print(history.history)
     return model
 
 
-# def testing(model, valid_dataset):
-#     pass
-#     # pred = model.predict(valid_dataset.take(5))
-#     # print(pred)
+def test(model_path, start_date, end_date, test_tf_record_folder_name, file_base, chat_num_ls):
+    for tmp_num in map(str, chat_num_ls):
+        test_tf_record_path = os.path.join(TF_RECORD_PATH, test_tf_record_folder_name, "%s_%s" % (file_base, tmp_num))
+        assert os.path.isdir(test_tf_record_path)
+        evaluate_model(model_path, test_tf_record_path, start_date, end_date)
 
 
 def prepare_dataset(date_ls, train_tf_record_folder_name, valid_tf_record_folder_name):
@@ -53,6 +64,7 @@ def prepare_dataset(date_ls, train_tf_record_folder_name, valid_tf_record_folder
     feature_ls = ["chat_content"]
     label_name = "label"
     padding = ({"chat_content": [SEQUENCE_MAX_LEN]}, [None])
+
     train_dataset = tfRecorder.get_dataset_from_path(train_tf_record_path, feature_ls, label_name=label_name,
                                                      start_date=date_ls[0], end_date=date_ls[1],
                                                      batch_size=args.batch_size, padding=padding)
@@ -65,31 +77,39 @@ def prepare_dataset(date_ls, train_tf_record_folder_name, valid_tf_record_folder
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="textCnn model..")
     parser.add_argument("-batch_size", "--batch_size", type=int, default=256, help="batch_size")
-    parser.add_argument("-epochs", "--epochs", type=int, default=1, help="epochs")
+    parser.add_argument("-epochs", "--epochs", type=int, default=40, help="epochs")
     parser.add_argument("-feature_size", "--feature_size", type=int, default=300, help="feature_size")
     parser.add_argument("-embedding_size", "--embedding_size", type=int, default=32, help="embedding_size")
     parser.add_argument("-classes_num", "--classes_num", type=int, default=2, help="classes_num")
     parser.add_argument("-filter_num", "--filter_num", type=int, default=16, help="filter_num")
     parser.add_argument("-filter_list", "--filter_list", type=str, default="3,4,5,6", help="filter_list")
-    parser.add_argument("-drop_out_ratio", "--drop_out_ratio", type=float, default=0.8, help="drop_out_ratio")
+    parser.add_argument("-drop_out_ratio", "--drop_out_ratio", type=float, default=0.5, help="drop_out_ratio")
     parser.add_argument("-validation_split", "--validation_split", type=float, default=0.1, help="validation_split")
     parser.add_argument("-result_dir", "--result_dir", type=str, default=os.path.join(PROJECT_DATA_DIR, "result"),
                         help="result_dir")
     parser.add_argument("-train_start_date", "--train_start_date", type=str, default="20190406",
                         help="train_start_date")
     parser.add_argument("-train_end_date", "--train_end_date", type=str, default="20190713", help="train_end_date")
-    parser.add_argument("-test_start_date", "--test_start_date", type=str, default="20190719", help="test_start_date")
-    parser.add_argument("-test_end_date", "--test_end_date", type=str, default="20190725", help="test_end_date")
+    parser.add_argument("-valid_start_date", "--valid_start_date", type=str, default="20190719",
+                        help="valid_start_date")
+    parser.add_argument("-valid_end_date", "--valid_end_date", type=str, default="20190725", help="valid_end_date")
+    parser.add_argument("-test_start_date", "--test_start_date", type=str, default="20190801", help="test_start_date")
+    parser.add_argument("-test_end_date", "--test_end_date", type=str, default="20190930", help="test_end_date")
     parser.add_argument("-train_tf_record_folder_name", "--train_tf_record_folder_name", type=str,
                         default="wechat_basic_feature", help="train_tf_record_folder_name")
     parser.add_argument("-valid_tf_record_folder_name", "--valid_tf_record_folder_name", type=str,
                         default="wechat_basic_feature", help="valid_tf_record_folder_name")
+    parser.add_argument("-test_tf_record_folder_name", "--test_tf_record_folder_name", type=str,
+                        default="wechat_basic_feature_bench_mark", help="test_tf_record_folder_name")
     parser.add_argument("-pos_sample_weight", "--pos_sample_weight", type=float,
                         default=40.0, help="pos_sample_weight")
+
+    parser.add_argument("-is_test", "--is_test", type=bool, default=False, help="is_test")
 
     args = parser.parse_args()
     print("Argument:", args, "\n")
 
+    # Prepare 结果输出目录
     if not os.path.isdir(args.result_dir):
         os.mkdir(args.result_dir)
     timestamp = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d-%H-%M")
@@ -97,15 +117,20 @@ if __name__ == '__main__':
     if not os.path.isdir(log_path):
         os.makedirs(log_path)
 
-    print("prepare train data and test data..")
-    date_ls = [args.train_start_date, args.train_end_date, args.test_start_date, args.test_end_date]
+    print("Prepare train data and test data..")
+    date_ls = [args.train_start_date, args.train_end_date, args.valid_start_date, args.valid_end_date]
     train_dataset, valid_dataset = prepare_dataset(date_ls, args.train_tf_record_folder_name,
                                                    args.valid_tf_record_folder_name)
 
     print("Training")
-    model_saved_path = os.path.join(args.result_dir, timestamp, "Model_Saved")
+    model_saved_path = os.path.join(args.result_dir, timestamp, "saved_model")
     vocab_size = data_helper.DataHelper().vocab_size
-    model = training(train_dataset, valid_dataset, vocab_size + 1, args.epochs, model_saved_path)
+    model = training(train_dataset, valid_dataset, vocab_size + 1, args.epochs, model_saved_path, log_path)
 
     print("Testing")
-    # testing(model, valid_dataset)
+    from data_process.make_bench_mark_data import chat_num_ls
+
+    file_base = "total_chat_num"
+    chat_num_ls = chat_num_ls
+    test(model_saved_path, args.test_start_date, args.test_end_date, args.test_tf_record_folder_name, file_base,
+         chat_num_ls)
